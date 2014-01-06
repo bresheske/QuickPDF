e0f50ec4a8f47a3bca73d997ee04d019491dfad5
bresheske <bresheske@gmail.com>
2014-01-06 10:43:29 -0600

    usings sorted.

--- QuickPDF.Core/Services/DefaultPDFCreator.cs
+++ QuickPDF.Core/Services/DefaultPDFCreator.cs
@@ -1,10 +1,6 @@
 ﻿using PdfSharp.Drawing;
 using PdfSharp.Pdf;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuickPDF.Core.Services
 {

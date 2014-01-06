d1b43556ee8f39eff5bfa4af21265465eb6da285
bresheske <bresheske@gmail.com>
2014-01-06 10:29:09 -0600

    everything working it seems. creating more tests.

--- QuickPDF.Core/Services/DefaultPDFCreator.cs
+++ QuickPDF.Core/Services/DefaultPDFCreator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,10 @@ using System.Threading.Tasks;
 
 namespace QuickPDF.Core.Services
 {
+    /// <summary>
+    /// Object is responsible for taking a list of images and converting it to a 
+    /// multi-page PDF Document.
+    /// </summary>
     public class DefaultPDFCreator : IPDFCreator
     {
         private readonly List<string> files;
@@ -15,16 +21,37 @@ namespace QuickPDF.Core.Services
             files = new List<string>();
         }
 
+        /// <summary>
+        /// Adds a file to the Image list.  
+        /// </summary>
+        /// <param name="file"></param>
         public void AddImage(string file)
         {
             files.Add(file);
         }
 
+        /// <summary>
+        /// Converts the image list into a multi-page PDF file.
+        /// </summary>
+        /// <param name="outputfile"></param>
         public void ProcessPDF(string outputfile)
         {
-            
+            var doc = new PdfDocument();
+            foreach (var f in files)
+            {
+                var p = doc.AddPage();
+                using (var gfx = XGraphics.FromPdfPage(p))
+                using (var img = XImage.FromFile(f))
+                {
+                    gfx.DrawImage(img, 0, 0);
+                }
+            }
+            doc.Save(outputfile);
         }
 
+        /// <summary>
+        /// Just clears out the file list.
+        /// </summary>
         public void Dispose()
         {
             files.Clear();
--- QuickPDF/Program.cs
+++ QuickPDF/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuickPDF.Core.Services;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,28 @@ namespace QuickPDF.Console
     {
         public static void Main(string[] args)
         {
-            // No need for arg parsing, just assuming file names are correct.
+            // TODO: IOC probably more appropriate here.
+            var pdf = new DefaultPDFCreator();
 
+            // First is output file.
+            var output = args.First();
 
+            // Rest is image files.
+            var files = args.Skip(1);
+
+            foreach (var f in files)
+            {
+                // If file is relative, use our environment path.
+                var file = f;
+                if (!Path.IsPathRooted(file))
+                    file = Environment.CurrentDirectory + @"\" + f;
+                // Add it to our list.
+                pdf.AddImage(f);
+            }
+
+            // Finish up.
+            pdf.ProcessPDF(output);
+            pdf.Dispose();
         }
     }
 }
